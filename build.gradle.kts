c736fc49ffdc52c471778b203af969ae983097c1
ChosenQuill <31707957+ChosenQuill@users.noreply.github.com>
2023-04-19 18:21:21 -0500

    Added Endpoints to create and get Todo entries

--- build.gradle.kts
+++ build.gradle.kts
@@ -19,9 +19,10 @@ repositories {
 }
 
 dependencies {
-	implementation("org.springframework.boot:spring-boot-starter-jdbc")
 	implementation("org.springframework.boot:spring-boot-starter-security")
 	implementation("org.springframework.boot:spring-boot-starter-web")
+	implementation("org.springframework.boot:spring-boot-starter-data-jpa")
+	implementation("com.h2database:h2")
 	annotationProcessor("org.springframework.boot:spring-boot-configuration-processor")
 	testImplementation("org.springframework.boot:spring-boot-starter-test")
 	testImplementation("org.springframework.security:spring-security-test")

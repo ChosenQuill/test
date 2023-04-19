cb0abbc71921ed56dc9236ebcd7d0e9b0dc5b313
ChosenQuill <31707957+ChosenQuill@users.noreply.github.com>
2023-04-19 18:48:14 -0500

    Add swagger docs

--- src/main/java/com/todoapp/plus/todoapp/plus/TodoBackendApplication.java
+++ src/main/java/com/todoapp/plus/todoapp/plus/TodoBackendApplication.java
@@ -1,7 +1,14 @@
 package com.todoapp.plus.todoapp.plus;
 
+import io.swagger.v3.oas.models.Components;
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.info.License;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class TodoBackendApplication {
@@ -10,4 +17,11 @@ public class TodoBackendApplication {
 		SpringApplication.run(TodoBackendApplication.class, args);
 	}
 
+	@Bean
+	public OpenAPI customOpenAPI(@Value("${springdoc.version}") String appVersion) {
+		return new OpenAPI()
+				.components(new Components())
+				.info(new Info().title("Todo API").version(appVersion)
+						.license(new License().name("Apache 2.0").url("http://springdoc.org")));
+	}
 }

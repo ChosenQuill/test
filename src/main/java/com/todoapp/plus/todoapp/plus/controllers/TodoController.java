3e55c5f898ea1ed4dcd938561c179115055d790a
Landry Norris <landry.norris@live.com>
2023-04-24 20:43:17 -0500

    Add ability to tie a reminder to a task

--- src/main/java/com/todoapp/plus/todoapp/plus/controllers/TodoController.java
+++ src/main/java/com/todoapp/plus/todoapp/plus/controllers/TodoController.java
@@ -1,8 +1,10 @@
 package com.todoapp.plus.todoapp.plus.controllers;
 
 import com.todoapp.plus.todoapp.plus.models.Category;
+import com.todoapp.plus.todoapp.plus.models.Reminder;
 import com.todoapp.plus.todoapp.plus.models.TodoModel;
 import com.todoapp.plus.todoapp.plus.repository.CategoryRepository;
+import com.todoapp.plus.todoapp.plus.repository.ReminderRepository;
 import com.todoapp.plus.todoapp.plus.repository.TodoRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
@@ -18,6 +20,9 @@ class TodoController {
     private TodoRepository repository;
     @Autowired
     private CategoryRepository categoryRepository;
+    @Autowired
+    private ReminderRepository reminderRepository;
+
     private final DateFormat dateFormat = new SimpleDateFormat("MM/dd/yyyy");
 
     @PostMapping("/todo")
@@ -59,7 +64,33 @@ class TodoController {
     }
 
     @GetMapping("/todo/{id}")
-    public TodoModel getTodo(@PathVariable Integer id) {
+    public TodoModel getTodo(@PathVariable int id) {
         return repository.getTodoById(id);
+    }
+
+    @PostMapping("/todo/{id}/reminder")
+    public void addReminder(@PathVariable int id,
+                            @RequestParam String name,
+                            @RequestParam(required = false) String dateString) {
+        TodoModel model = repository.getTodoById(id);
+
+        Date date;
+        if(dateString == null) {
+            date = model.getDueDate();
+        } else {
+            try {
+                date = dateFormat.parse(dateString);
+            } catch (ParseException e) {
+                throw new IllegalArgumentException();
+            }
+        }
+
+        Reminder reminder = new Reminder();
+        reminder.setReminderDate(date);
+        reminder.setName(name);
+
+        reminderRepository.save(reminder);
+
+        model.addReminder(reminder);
     }
 }
